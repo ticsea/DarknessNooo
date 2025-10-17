ff4fab77aa56240a783fd7f651ff8a694b060cb0
ticsea <ticseaatlan@gmail.com>
2025-10-18 07:34:23 +0800

    fix: fix blindness effect doesnt block the sky.

--- src/main/java/ticsea/darknessnooo/mixin/LevelRendererMixin.java
+++ src/main/java/ticsea/darknessnooo/mixin/LevelRendererMixin.java
@@ -1,7 +1,13 @@
 package ticsea.darknessnooo.mixin;
 
+import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,10 +16,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 /*Block Sky Effect
 * 天空遮效果*/
 //todo exclusive blight
+@OnlyIn(Dist.CLIENT)
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin implements ResourceManagerReloadListener, AutoCloseable {
-    @Inject(method = "doesMobEffectBlockSky", at = @At("HEAD"), cancellable = true)
-    private void doesMobEffectBlockSky(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+    @Inject(method = "doesMobEffectBlockSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getEntity()Lnet/minecraft/world/entity/Entity;", shift = At.Shift.BY, by = 3), cancellable = true)
+    private void doesMobEffectBlockSky(Camera pCamera, CallbackInfoReturnable<Boolean> cir) {
+        Entity entity = pCamera.getEntity();
+        cir.setReturnValue(((LivingEntity) entity).hasEffect(MobEffects.BLINDNESS));
     }
+
+   /* @Inject(method = "doesMobEffectBlockSky", at = @At(value = "HEAD"), cancellable = true)
+    private void d2(Camera pCamera, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }*/
 }
